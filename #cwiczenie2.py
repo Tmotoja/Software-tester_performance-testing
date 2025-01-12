962825c4ee45cfa0c4a19989ad93abcaeb023122
Tmotoja <suwak-obwinienie.9n@icloud.com>
2025-01-12 11:43:47 +0100

    Add files via upload

---
+++ #cwiczenie2.py
@@ -0,0 +1,14 @@
+#cwiczenie2
+
+def stworz_album(artysta, album, liczba_utworow):
+
+
+
+    slownik1 = {artysta: "Young Multi", album: "Trap After Death"}
+    slownik2 = {artysta: "Rihanna", album: "Diamonds", liczba_utworow: 12}
+    slownik3 = {artysta: "Sia", album: "Unstoppable"}
+
+    return album1, album2, album3
+
+albumy = stworz_album("Rihanna", "Diamonds", 12)
+print(albumy)
